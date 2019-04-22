fdaf301c1d7e9f47cbea96b1dc9b3e04d026418b
Ramon <ramon.oliveira@devell.com.br>
2019-04-22 10:05:45 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IntroPageModule } from '../pages/intro/intro.module';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    IntroPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
